ae9967ed07b856df5b8e97e19b92cd7f4d85e0df
yunekorea <kooopooo@naver.com>
2024-03-04 14:24:40 +0900

    Added a get_trace function

--- trace_parser.c
+++ trace_parser.c
@@ -1,1 +1,30 @@
 #include "trace_parser.h"
+
+Trace get_filename(void)
+{
+  Trace trace;
+  printf("I/O trace file name : ");
+  scanf("%s", trace.filename);
+  return trace;
+}
+
+int get_trace(Trace *trace, Request *request)
+{
+  static char buffer[200];
+  u64 timestamp;
+  u32 lpn, offset, size;
+  u8 ope;
+  if(feof(trace->trfile))
+    return -1;
+  
+  fgets(buffer, 200, trace->trfile);
+  sscanf(buffer, "%lld %d %d %d %d", &timestamp, &lpn, &offset, &size, &ope);
+  request = malloc(sizeof(Request));
+  request->timestamp = timestamp;
+  request->lpn = lpn;
+  request->offset = offset;
+  request->size = size;
+  request->operation = ope;
+
+  return 1;
+}
--- trace_parser.h
+++ trace_parser.h
@@ -1,3 +1,29 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
 
+typedef unsigned long long u64;
+typedef unsigned long u32;
+typedef char u8;
+
+struct _IO_FILE;
+typedef struct _IO_FILE FILE;
+
+typedef struct _Request {
+  u64 timestamp;
+  u32 lpn;
+  u32 offset;
+  u32 size;
+  u8 operation;
+}Request;
+
+typedef struct _Trace {
+  char filename[50];
+  FILE *trfile;
+} Trace;
+
+
+Trace get_filename(void);
+int get_trace(Trace *trace, Request *request);
 
