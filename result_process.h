57bbfced532a3cd5b254b5a522ffaf4951b7d0c3
yunekorea <kooopooo@naver.com>
2024-03-11 20:26:08 +0900

    fixed bugs #6

--- result_process.h
+++ result_process.h
@@ -1,10 +1,15 @@
 #include "types.h"
 #include "trace_parser.h"
 
+#ifndef _RESULT_PROCESS
+#define _RESULT_PROCESS
+
 typedef struct _Op_result {
   u16 tag;
   u64 time_spent;
 } Op_result;
+
+#endif
 
 int resultfile_open(FILE *res_file);
 int save_fined_to_file(FILE *res_file, Request *request);
