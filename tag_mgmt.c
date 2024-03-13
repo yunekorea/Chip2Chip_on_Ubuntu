c5b83c313e7581ec0b32bfef12a2ab77ae604e59
yunekorea <kooopooo@naver.com>
2024-03-13 13:30:05 +0900

    added some printf functions to check what's going on. #2

--- tag_mgmt.c
+++ tag_mgmt.c
@@ -4,7 +4,7 @@ Request *tags[128];
 
 int allocate_tag(Request *request)
 {
-  static int tags_pointer = 0;
+  static u8 tags_pointer = 0;
   while(tags[tags_pointer] != NULL) {
     tags_pointer++;
     if(tags_pointer > 127)
@@ -24,11 +24,12 @@ Request *save_result_to_request(Op_result *result)
 {
   printf("save result to request\n");
   Request *fin_req;
-  tags[(int)result->tag]->result_time = result->time_spent;
-  
-  fin_req = tags[result->tag];
-  //free(result);
-  tags[(int)result->tag] = NULL;
+  tags[(u8)result->tag]->result_time = result->time_spent;
+  printf("SRTR : time save\n");
+  fin_req = tags[(u8)result->tag];
+
+  free(result);
+  tags[(u8)result->tag] = NULL;
 
   return fin_req;
 }
