d54c800165336794ca7e294c66f67339425392f3
yunekorea <kooopooo@naver.com>
2024-03-20 14:31:08 +0900

    disabled file saver thread.

--- main.c
+++ main.c
@@ -98,11 +98,11 @@ int main(void)
 
   thread_id = pthread_create(&pthread[0], NULL, thread_command_generator, (void*)thread_args);
   thread_id = pthread_create(&pthread[1], NULL, thread_result_receiver, (void*)thread_args);
-  thread_id = pthread_create(&pthread[2], NULL, thread_file_saver, (void*)thread_args);
+  //thread_id = pthread_create(&pthread[2], NULL, thread_file_saver, (void*)thread_args);
 
   pthread_join(pthread[0], (void**)&status);
   pthread_join(pthread[1], (void**)&status);
-  pthread_join(pthread[2], (void**)&status);
+  //pthread_join(pthread[2], (void**)&status);
 
   fclose(trace->trfile);
   fclose(res_file);
